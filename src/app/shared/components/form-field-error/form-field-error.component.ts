309be504aa4b796199a77b262b3d6c4d8da2beba
Lizandro Kenedy <lizandrokenedy@gmail.com>
2019-09-17 18:05:32 -0400

    commit

---
+++ src/app/shared/components/form-field-error/form-field-error.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-field-error',
+  template: `
+    <p class="text-danger">
+      {{errorMessage}}
+    </p>
+  `,
+  styleUrls: ['./form-field-error.component.css']
+})
+export class FormFieldErrorComponent implements OnInit {
+
+  @Input('form-control') formControl: FormControl
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public get errorMessage(): string{
+    if(this.mustShowErrorMessage()){
+      return this.getErrorMessage()
+    } else {
+      return null
+    }
+  }
+
+  private mustShowErrorMessage(): boolean {
+    return this.formControl.invalid && this.formControl.touched
+  }
+
+  private getErrorMessage(): string | null {
+    if(this.formControl.errors.required){
+      return "Campo obrigatório"
+    }
+    else if(this.formControl.errors.email) {
+      return "E-mail inválido"
+    }
+    else if(this.formControl.errors.minlength) {
+      const requiredLength = this.formControl.errors.minlength.requiredLength
+      return `Minímo ${requiredLength} caracteres`
+    }
+    else if(this.formControl.errors.maxlength) {
+      const requiredLength = this.formControl.errors.maxlength.requiredLength
+      return `Máximo ${requiredLength} caracteres`
+    }
+
+  }
+
+}
